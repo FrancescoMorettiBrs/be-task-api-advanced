d9a7dc148dac0f47f8666fae60ec5ebddaa27d5c
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2026-01-15 05:46:04 +0100

    feat: creata rotta Show

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,6 +1,6 @@
 import pool from "../db/connection.js";
 
-export async function getAllTasks(req, res) {
+export async function getAllTasks(req, res, next) {
   try {
     const [rows] = await pool.query("SELECT * FROM tasks");
     res.json({
@@ -11,9 +11,30 @@ export async function getAllTasks(req, res) {
   } catch (error) {
     console.error("Errore nel recupero delle task", error.message);
 
-    res.status(500).json({
-      success: false,
-      message: "Errore nel recupero delle task",
+    next(error);
+  }
+}
+
+export async function getTaskById(req, res, next) {
+  try {
+    const { id } = req.params;
+
+    const [rows] = await pool.query("SELECT * FROM tasks WHERE id = ?", [id]);
+
+    // Se non esiste una task con quell'id
+    if (rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Task non trovata",
+      });
+    }
+
+    // Se trovo la task
+    res.json({
+      success: true,
+      data: rows[0],
     });
+  } catch (err) {
+    next(err);
   }
 }
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllTasks } from "../controllers/tasks.controller.js";
+import { getAllTasks, getTaskById } from "../controllers/tasks.controller.js";
 
 const router = express.Router();
 
 // GET /tasks
 router.get("/", getAllTasks);
+router.get("/:id", getTaskById);
 
 export default router;
